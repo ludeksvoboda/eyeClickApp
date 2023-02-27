1717c20866750e4bc64025321abbe385cf2a45a9
ludek <ludek.svoboda@abtera.cz>
2023-02-27 18:09:27 +0100

    cleanup, its working quite ok at the evening, Y coordinate had to be flipped

--- app_messy.py
+++ app_messy.py
@@ -21,6 +21,8 @@ import pygame.camera
 import requests
 import cv2
 from pynput.mouse import Controller
+import pandas as pd
+
 
 SAVEDIR = "/home/lu/MLData/EyeClickApp/tests/"
 url = "http://10.0.0.21:8080/predictions/effnet"
@@ -30,12 +32,24 @@ pygame.camera.init()
 cam = pygame.camera.Camera("/dev/video1", (640, 480))
 cam.start()
     
+i = 0
+eval_data_list = []
 while True:
     img = pygame.surfarray.array3d(cam.get_image()).swapaxes(0,1)
+    # image_path = SAVEDIR + "test_"+ str(i) +".png"
+    # save_image(img, image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.imencode('.png', img)[1].tobytes()
     response_bytes = requests.put(url, data=img)
     data = response_bytes.text
+    # print(data)
     data = json.loads(data)
     position_adjusted = (data["data"][0][0]+ 960, -data["data"][0][1] + 540)
     mouse.position = position_adjusted
+    # eval_data_list.append([image_path, data["data"][0][0], data["data"][0][1]])
+    # print(i)
+    # if i == 100:
+    #     eval_df = pd.DataFrame(eval_data_list, columns=["path", "x", "y"])
+    #     eval_df.to_csv("eval_data.csv")
+    #     break
+    # i += 1
